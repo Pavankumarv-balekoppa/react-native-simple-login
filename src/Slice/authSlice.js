b76f6d13d90213cd1fb77319a2a8fb1f6d645f0b
pavankumarv-balekoppa <pavankumarv271@gmail.com>
2024-09-16 16:50:08 +0530

    added store in index

--- src/Slice/authSlice.js
+++ src/Slice/authSlice.js
@@ -4,6 +4,7 @@ import { storeUserData } from '../constants/helper';
 const initialState = {
   isAuthenticated: false,
   user: null,
+  isLoading : false,
 };
 
 
@@ -13,22 +14,27 @@ const authSlice = createSlice({
   initialState,
   reducers: {
     login: (state, action) => {
+      state.isLoading = true;
       const {email, password} = action.payload;
-      if (email === 'pavan.v@adcuratio.com' && password === 'pavan@123') {
+      // if (email === 'pavan.v@adcuratio.com' && password === 'pavan@123') {
+      if (email === 'p' && password === 'p') {
         state.isAuthenticated = true;
         state.user = action.payload; // Set user data
         storeUserData(action.payload);
       } else {
         alert('Invalid credentials');
       }
+      state.isLoading = false;
     },
     logout: state => {
+      state.isLoading = true;
       state.isAuthenticated = false;
       state.user = null; // Clear user data
       storeUserData(null);
+      state.isLoading = false;
     },
   },
 });
 
-export const {login, logout} = authSlice.actions;
+export const {login, logout, setLoading} = authSlice.actions;
 export default authSlice.reducer;
